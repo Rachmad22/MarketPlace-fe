2ed7b4ea3d501566b047126f5dbcf8d8159cc884
Riyadh <albarrriyadh1112@gmail.com>
2023-02-14 16:00:53 +0800

    [ADD] env, components folder, etc

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+import styles from '@/styles/Home.module.scss'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+const Home = () => {
   return (
     <>
       <Head>
@@ -121,3 +121,5 @@ export default function Home() {
     </>
   )
 }
+
+export default Home;
