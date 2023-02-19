a0b9a38932644aadc028bf32819d96f83ff0110e
Rachmad22 <rachmadnur22@gmail.com>
2023-02-20 03:06:53 +0700

    [FIX] ESlint

--- src/pages/auth/login/index.js
+++ src/pages/auth/login/index.js
@@ -23,6 +23,7 @@ const Login = () => {
   const data = useSelector((state) => state.login);
   const loginSubmit = () => {
     setIsLoading(true);
+    console.log(data)
     axios
       .post(`${process.env.NEXT_PUBLIC_API_URL}/auth/login`, data)
       .then((res) => {
--- src/stores/index.js
+++ src/stores/index.js
@@ -1,4 +1,6 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import login from "./reducer/login";
+import register from "./reducer/register";
 import profile from "./reducer/profile";
 import { persistReducer } from "redux-persist";
 import { combineReducers } from "redux";
@@ -6,7 +8,7 @@ import storage from "redux-persist/lib/storage";
 import { encryptTransform } from "redux-persist-transform-encrypt";
 
 const reducers = combineReducers({
-  profile,
+  profile, login, register,
 });
 
 const persistConfig = {
