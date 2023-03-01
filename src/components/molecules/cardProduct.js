82d3d5e4382615e537c620606fe6f8c598577598
Rachmad22 <rachmadnur22@gmail.com>
2023-03-01 15:10:42 +0700

    [FIX] integration

--- src/components/molecules/cardProduct.js
+++ src/components/molecules/cardProduct.js
@@ -1,23 +1,33 @@
+
 import styles from "@/styles/components/CardProduct.module.scss";
 import jacket from "public/images/jacket.png";
 // import rating from "public/images/rating.svg";
+import Link from "next/link";
 
-const CardProduct = () => {
- return (
-  <>
-   <div class="container">
-    <div class={`card ${styles.cardProduct}`}>
-     <img src={jacket.src} class="card-img-top" alt="Jacket" />
-     <div class="card-body">
-      <h5>Men&apos;s formal suit - Black & White</h5>
-      <p class={styles.price}>$ 40.0</p>
-      <p class={styles.store}>Zalora Cloth</p>
-      {/* <img src={rating.src} /> */}
-     </div>
-    </div>
-   </div>
-  </>
- );
-};
+const CardProduct = ({ item }) => {
+  return (
+    <>
+      <div class="container">
+        <div class={`card ${styles.cardProduct}`}>
+          <Link href={`/detail/${item?.id}`}>
+            <img
+              src={jacket.src}
+              class="card-img-top"
+              alt={item?.product_name}
+            />
+          </Link>
+          <div class="card-body">
+            <Link href={`/detail/${item?.id}`}>
+              <h5>{item?.product_name}</h5>
+            </Link>
+            <p class={styles.price}>$ {item?.price}</p>
+            <p class={styles.store}>{item?.store_name}</p>
+            {/* <img src={rating.src} alt="rating" /> */}
+          </div>
+        </div>
+      </div>
+      </>
 
-export default CardProduct;
+      )}
+
+      export default CardProduct;
